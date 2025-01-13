3b197932be5ba190e9132ea70b3796e607d53678
StephAlvarado <stephya16@gmail.com>
2025-01-13 01:00:36 -0500

    week 6 lesson

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,18 +4,19 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 //routes 
-app.get('/hello', (req, res) =>{
-res.send('Task Manager App')
-})
-
 
 app.use('/api/v1/tasks', tasks)
 
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 // app.get('/api/v1/tasks') - get all the tasks
 // app.post('/api/v1/tasks') - create a new task
@@ -24,7 +25,7 @@ app.use('/api/v1/tasks', tasks)
 // app.delete('/api/v1/tasks/:id') -delete task
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 const start = async () =>{
